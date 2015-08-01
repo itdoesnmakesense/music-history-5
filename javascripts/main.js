c131ef612817f594cc28cba6dfe0eb8e655df9e0
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2015-08-01 12:28:26 -0500

    fixed the delete buttons. still need to work on showing the second list when more button is clicked

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,60 +1,76 @@
 requirejs(["dom-access", "populate-songs", "get-more-songs"], 
   function(dom,pop,more){
-      console.log("dom", dom);
 
       pop.setSongs();
-      console.log("pop", pop.getSongs());
+    
 
       more.setSongs();
-      console.log("more", more.getSongs());
 
-  function firstSongList(data) {
-    var divResponse = $(".actual-songs");
-    var songInfo = "";
-    var currentSong;
-    var popSongs = pop.getSongs();
+      // function firstSongList(data) {
+        var divResponse = $(".actual-songs");
+        var songInfo = "";
+        var currentSong;
+        var popSongs = pop.getSongs();
 
-        for (var i = 0; i <= popSongs.length; i++ ) {
-        currentSong = popSongs[i];
-       
-        songInfo += "<div id= 'song-container'>";
-        songInfo += "<div id='song-title' class='song-info'><p>" + currentSong.name +" | "+ currentSong.artists + "</p></div>";
-        songInfo += "<div id='song-album' class='song-info'><p>" + currentSong.album.name + " | " + currentSong.album.year + "</p>" + "</div>";
-        songInfo +=  "<button id='delete'>"+ "Delete" + "</button>"+"</div>";
-        divResponse.html(songInfo);
-  }
+            for (var i = 0; i <= popSongs.length; i++ ) {
+            currentSong = popSongs[i];
+           
+            songInfo += "<div id= 'song-container'>";
+            songInfo += "<div id='song-title' class='song-info'><p>" + currentSong.name +" | "+ currentSong.artists + "</p></div>";
+            songInfo += "<div id='song-album' class='song-info'><p>" + currentSong.album.name + " | " + currentSong.album.year + "</p>" + "</div>";
+            songInfo +=  "<button class='deleteButton'>"+ "Delete" + "</button>"+"</div>";
+            divResponse.html(songInfo);
 
-}
-return firstSongList();
+              $('button').click(function(){
+              $(this).closest('#song-container').remove();
+              });
+            }
+          // }
+    // return firstSongList();
 
+      
+          // function secondSongList(data){
 
-function secondSongList(data){
             var songInfo = "";
             var currentSong;
             var buttonInfo = $("#moreSongs");
             var getMoreSongs = more.getSongs();
 
-        for (var i = 0; i <= getMoreSongs.length; i++ ) {
-        currentSong = getMoreSongs[i];
-        
+              for (var i = 0; i <= getMoreSongs.length; i++ ) {
+              currentSong = getMoreSongs[i];
+              
 
-        songInfo += "<div class= 'song-container'>";
-        songInfo += "<div class='song-title'><p>" + currentSong.name +" | "+ currentSong.artists + "</p></div>";
-        songInfo += "<div class = 'song-info'>";
-        songInfo += "<p>" + currentSong.album.name + " | " + currentSong.album.year + "</p>" + "</div>";
-        songInfo +=  "</div>";
-        buttonInfo.html(songInfo);
-        }
-        return secondSongList();
-        }
+              songInfo += "<div class= 'song-container'>";
+              songInfo += "<div class='song-title'><p>" + currentSong.name +" | "+ currentSong.artists + "</p></div>";
+              songInfo += "<div class = 'song-info'>";
+              songInfo += "<p>" + currentSong.album.name + " | " + currentSong.album.year + "</p>" + "</div>";
+              songInfo +=  "</div>";
+              buttonInfo.html(songInfo);
 
-});
+                $('button').click(function(){
+                $(this).closest('#song-container').remove();
+                });
+              }
 
+              
+            });
 
+            // }
 
-$('#moreButton').on('click',function(){
-  console.log('click');
-});
+      // return secondSongList();
+// $('#moreButton').on('click',function(){
+//                 console.log('click');
+//                 return secondSongList();
+//                 console.log(secondSongList());
+//                  });
+
+ // $('#moreButton').on('click',function(){
+ //    return getMoreSongs;
+ //  });
+
+// $('#moreButton').on('click',function(){
+//   return secondSongList();
+// });
 
 
 
